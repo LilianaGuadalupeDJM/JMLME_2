1dbe47e384ae266b813b5e75630dc4dfe12232d5
Liliana25072003 <lilianadejesusmartinez3@gmail.com>
2024-07-24 21:35:18 -0600

    yaaaa

--- src/pages/Admision/AdmisionesPage.jsx
+++ src/pages/Admision/AdmisionesPage.jsx
@@ -1,148 +1,150 @@
 import React, { useState, useEffect } from 'react';
-import { message, Button, Space, Table, Divider } from 'antd';
+import { message, Button, Space, Table } from 'antd';
 import { getAdmisiones, addAdmision, editAdmision, deleteAdmision } from '../../services/admision';
 import AdmisionesModals from './AdmisionesModals';
-import { PlusOutlined, EditOutlined, DeleteOutlined, ReloadOutlined } from '@ant-design/icons';
+import { PlusOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import './AdmisionesPage.css';
 import Nav from '../../components/Nav';
 
 const AdmisionesPage = () => {
-    const [admisiones, setAdmisiones] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [selectedAdmision, setSelectedAdmision] = useState(null);
-    const [isAddModalVisible, setIsAddModalVisible] = useState(false);
-    const [isEditModalVisible, setIsEditModalVisible] = useState(false);
-    const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const user = JSON.parse(localStorage.getItem('user')); // Asumiendo que los datos del usuario están en localStorage
 
-    useEffect(() => {
-        fetchAdmisiones();
-    }, []);
+  const [admisiones, setAdmisiones] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedAdmision, setSelectedAdmision] = useState(null);
+  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
+  const [isViewModalVisible, setIsViewModalVisible] = useState(false);
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
 
-    const fetchAdmisiones = async () => {
-        try {
-            const data = await getAdmisiones();
-            setAdmisiones(data);
-            setLoading(false);
-        } catch (error) {
-            message.error('Error al obtener admisiones');
-            setLoading(false);
-        }
-    };
+  useEffect(() => {
+    fetchAdmisiones();
+  }, []);
 
-    const handleAddAdmision = async (values) => {
-        try {
-            await addAdmision(values.nombre, values.activo);
-            message.success('Admision agregada exitosamente');
-            fetchAdmisiones(); // Actualiza la lista después de agregar
-            setIsAddModalVisible(false);
-        } catch (error) {
-            message.error('Error al agregar admision');
-        }
-    };
+  const fetchAdmisiones = async () => {
+    try {
+      const data = await getAdmisiones();
+      setAdmisiones(data);
+      setLoading(false);
+    } catch (error) {
+      message.error('Error al obtener admisiones');
+      setLoading(false);
+    }
+  };
 
-    const handleEditAdmision = async (values) => {
-        try {
-            await editAdmision(selectedAdmision._id, values.nombre, values.activo);
-            message.success('Admision editada exitosamente');
-            fetchAdmisiones(); // Actualiza la lista después de editar
-            setIsEditModalVisible(false);
-        } catch (error) {
-            message.error('Error al editar admision');
-        }
-    };
+  const handleAddAdmision = async (values) => {
+    try {
+      await addAdmision(values.nombre, values.activo);
+      message.success('Admision agregada exitosamente');
+      fetchAdmisiones(); // Actualiza la lista después de agregar
+      setIsAddModalVisible(false);
+    } catch (error) {
+      message.error('Error al agregar admision');
+    }
+  };
 
-    const handleDeleteAdmision = async () => {
-        try {
-            await deleteAdmision(selectedAdmision._id);
-            message.success('Admision eliminada exitosamente');
-            fetchAdmisiones(); // Actualiza la lista después de eliminar
-            setIsDeleteModalVisible(false);
-        } catch (error) {
-            message.error('Error al eliminar admision');
-        }
-    };
+  const handleEditAdmision = async (values) => {
+    try {
+      await editAdmision(selectedAdmision._id, values.nombre, values.activo);
+      message.success('Admision editada exitosamente');
+      fetchAdmisiones(); // Actualiza la lista después de editar
+      setIsEditModalVisible(false);
+    } catch (error) {
+      message.error('Error al editar admision');
+    }
+  };
 
-    const columns = [
-        { title: 'Nombre', dataIndex: 'nombre', key: 'nombre', align: 'center' },
-        { title: 'Activo', dataIndex: 'activo', key: 'activo', align: 'center', render: (activo) => (activo ? 'Sí' : 'No') },
-        {
-            title: 'Acciones',
-            key: 'acciones',
-            align: 'center',
-            render: (text, record) => (
-                <Space>
-                    <Button onClick={() => setIsEditModalVisible(true)}>Editar</Button>
-                    <Button onClick={() => setIsDeleteModalVisible(true)}>Eliminar</Button>
-                </Space>
-            )
-        }
-    ];
+  const handleDeleteAdmision = async () => {
+    try {
+      await deleteAdmision(selectedAdmision._id);
+      message.success('Admision eliminada exitosamente');
+      fetchAdmisiones(); // Actualiza la lista después de eliminar
+      setIsDeleteModalVisible(false);
+    } catch (error) {
+      message.error('Error al eliminar admision');
+    }
+  };
 
-    const rowSelection = {
-        type: 'radio',
-        onChange: (selectedRowKeys, selectedRows) => {
-            setSelectedAdmision(selectedRows[0]);
-        },
-        selectedRowKeys: selectedAdmision ? [selectedAdmision._id] : [],
-    };
+  const columns = [
+    { title: 'Nombre', dataIndex: 'nombre', key: 'nombre', align: 'center' },
+    { title: 'Activo', dataIndex: 'activo', key: 'activo', align: 'center', render: (activo) => (activo ? 'Sí' : 'No') },
+    {
+      title: 'Acciones',
+      key: 'acciones',
+      align: 'center',
+      render: (text, record) => (
+        <Space>
+          <Button onClick={() => { setSelectedAdmision(record); setIsViewModalVisible(true); }}>Ver</Button>
+          <Button onClick={() => { setSelectedAdmision(record); setIsEditModalVisible(true); }}>Editar</Button>
 
-    return (
-        <div>
-            <Nav
-                greeting="Admisiones"
-                // No es necesario el botón de cierre de sesión en esta página
-            />
-            <Divider />
-            <div style={{ maxHeight: '600px', overflowY: 'auto' }}>
-                <Space className="action-buttons">
-                    <Button
-                        type="text"
-                        icon={<PlusOutlined style={{ color: '#01859a' }} />}
-                        onClick={() => setIsAddModalVisible(true)}
-                    />
-                    <Button
-                        type="text"
-                        icon={<EditOutlined style={{ color: '#01859a' }} />}
-                        onClick={() => setIsEditModalVisible(true)}
-                        disabled={!selectedAdmision}
-                    />
-                    <Button
-                        type="text"
-                        icon={<DeleteOutlined style={{ color: '#01859a' }} />}
-                        onClick={() => setIsDeleteModalVisible(true)}
-                        disabled={!selectedAdmision}
-                    />
-                    <Button
-                        type="text"
-                        icon={<ReloadOutlined style={{ color: '#01859a' }} />}
-                        onClick={() => window.location.reload()}
-                    />
-                </Space>
+        </Space>
+      )
+    }
+  ];
 
-                <Table
-                    dataSource={admisiones}
-                    columns={columns}
-                    rowKey="_id"
-                    loading={loading}
-                    rowSelection={rowSelection}
-                    className="custom-table"
-                />
+  const rowSelection = {
+    type: 'radio',
+    onChange: (selectedRowKeys, selectedRows) => {
+      setSelectedAdmision(selectedRows[0]);
+    },
+    selectedRowKeys: selectedAdmision ? [selectedAdmision._id] : [],
+  };
 
-                <AdmisionesModals
-                    isAddModalVisible={isAddModalVisible}
-                    setIsAddModalVisible={setIsAddModalVisible}
-                    handleAddAdmision={handleAddAdmision}
+  return (
+    <div>
+      <Nav />
+      <h1>Admisiones</h1>
 
-                    isEditModalVisible={isEditModalVisible}
-                    setIsEditModalVisible={setIsEditModalVisible}
-                    handleEditAdmision={handleEditAdmision}
+      <Space className="action-buttons">
+        <Button
+          type="text"
+          icon={<PlusOutlined style={{ color: '#01859a' }} />}
+          onClick={() => setIsAddModalVisible(true)}
+        />
+        <Button
+          type="text"
+          icon={<EditOutlined style={{ color: '#01859a' }} />}
+          onClick={() => selectedAdmision ? setIsEditModalVisible(true) : alert("Selecciona una admision para editar.")}
+          disabled={!selectedAdmision}
+        />
+        <Button
+          type="text"
+          icon={<DeleteOutlined style={{ color: '#01859a' }} />}
+          onClick={() => setIsDeleteModalVisible(true)}
+          disabled={!selectedAdmision}
+        />
+      </Space>
 
-                    isDeleteModalVisible={isDeleteModalVisible}
-                    setIsDeleteModalVisible={setIsDeleteModalVisible}
-                    handleDeleteAdmision={handleDeleteAdmision}
-                />
-            </div>
-        </div>
-    );
+      <div className="scrollable-table-container">
+        <Table
+          dataSource={admisiones}
+          columns={columns}
+          rowKey="_id"
+          loading={loading}
+          rowSelection={rowSelection}
+          className="custom-table"
+        />
+      </div>
+
+      <AdmisionesModals
+        isAddModalVisible={isAddModalVisible}
+        setIsAddModalVisible={setIsAddModalVisible}
+        handleAddAdmision={handleAddAdmision}
+
+        isViewModalVisible={isViewModalVisible}
+        setIsViewModalVisible={setIsViewModalVisible}
+        selectedAdmision={selectedAdmision}
+
+        isEditModalVisible={isEditModalVisible}
+        setIsEditModalVisible={setIsEditModalVisible}
+        handleEditAdmision={handleEditAdmision}
+
+        isDeleteModalVisible={isDeleteModalVisible}
+        setIsDeleteModalVisible={setIsDeleteModalVisible}
+        handleDeleteAdmision={handleDeleteAdmision}
+      />
+    </div>
+  );
 };
 
 export default AdmisionesPage;
