f1c3c618af002cb597a04d7a7770692a71593585
LilianaGuadalupeDJM <114377676+LilianaGuadalupeDJM@users.noreply.github.com>
2024-08-19 00:37:53 -0600

    yaaa2

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,4 +1,4 @@
-import { Divider, Table, Tag } from 'antd';
+import { Divider, Table, Tag, Layout } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../hooks/useAuth';
 import BotonesCrudUsuario from '../../components/BotonesCrudUsuario';
@@ -7,9 +7,64 @@ import { usersService } from '../../services/users';
 import Sidebar from '../../components/SiderBar';
 import './index.css';
 
+const { Content } = Layout;
+
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: '_id',
+        key: 'id',
+        align: 'center',
+    },
+    {
+        title: 'Nombre de Usuario',
+        dataIndex: 'username',
+        key: 'username',
+        align: 'center',
+    },
+    {
+        title: 'Correo Electrónico',
+        dataIndex: 'email',
+        key: 'email',
+        align: 'center',
+    },
+    {
+        title: 'Roles',
+        dataIndex: 'roles',
+        key: 'roles',
+        align: 'center',
+        render: (roles, record) => (
+            <span>
+                {roles.length > 0 ? (
+                    roles.map(role => (
+                        <Tag color={getColor(role._id)} key={role._id}>
+                            {getRoleName(role._id)}
+                        </Tag>
+                    ))
+                ) : (
+                    <Tag color="blue">Customer</Tag>
+                )}
+            </span>
+        ),
+    },
+    {
+        title: 'Fecha de Creación',
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+        align: 'center',
+    },
+    {
+        title: 'Fecha de Actualización',
+        dataIndex: 'updatedAt',
+        key: 'updatedAt',
+        align: 'center',
+    },
+];
+
 const Usuarios = () => {
     const { user } = useAuth();
     const [users, setUsers] = useState([]);
+    const [roles, setRoles] = useState([]);
     const [selectedUserId, setSelectedUserId] = useState(null);
 
     const token = storageController.getToken();
@@ -25,81 +80,16 @@ const Usuarios = () => {
         }),
     };
 
-    const columns = [
-        {
-            title: 'ID',
-            dataIndex: '_id',
-            align: 'center',
-        },
-        {
-            title: 'Nombre de Usuario',
-            dataIndex: 'username',
-            align: 'center',
-        },
-        {
-            title: 'Correo Electrónico',
-            dataIndex: 'email',
-            align: 'center',
-        },
-        {
-            title: 'Roles',
-            dataIndex: 'roles',
-            align: 'center',
-            render: (roles) => (
-                <span>
-                    {roles.length > 0 ? (
-                        roles.map(role => (
-                            <Tag color={getColor(role._id)} key={role._id}>
-                                {getRoleName(role._id)}
-                            </Tag>
-                        ))
-                    ) : (
-                        <Tag color="blue">Customer</Tag>
-                    )}
-                </span>
-            ),
-        },
-        {
-            title: 'Fecha de Creación',
-            dataIndex: 'createdAt',
-            align: 'center',
-        },
-        {
-            title: 'Fecha de Actualización',
-            dataIndex: 'updatedAt',
-            align: 'center',
-        },
-    ];
-
-    const getRoleName = (roleId) => {
-        switch (roleId) {
-            case '666b5995e842a28618ccfc94':
-                return 'Customer';
-            case '666b5995e842a28618ccfc95':
-                return 'Administrador';
-            default:
-                return 'Desconocido';
-        }
-    };
-
-    const getColor = (roleId) => {
-        switch (roleId) {
-            case '666b5995e842a28618ccfc94':
-                return 'blue';
-            case '666b5995e842a28618ccfc95':
-                return 'yellow';
-            default:
-                return 'default';
-        }
-    };
-
     const fetchUsers = async () => {
         try {
             const data = await usersService.getAllUsers(token);
             const usersWithKey = data.map(user => ({
                 ...user,
                 key: user._id,
-                roles: user.roles.map(role => ({ _id: role, name: getRoleName(role) })) // Asegúrate de que roles sea un array de objetos con `_id` y `name`
+                roles: user.roles.map(roleId => ({
+                    _id: roleId,
+                    name: getRoleName(roleId)
+                }))
             }));
             setUsers(usersWithKey);
         } catch (error) {
@@ -107,14 +97,35 @@ const Usuarios = () => {
         }
     };
 
+    const fetchRoles = async () => {
+        try {
+            const data = await rolesService.getRoles(token);
+            setRoles(data);
+        } catch (error) {
+            console.error('Error al obtener roles', error);
+        }
+    };
+
     useEffect(() => {
+        fetchRoles();
         fetchUsers();
     }, [token]);
 
+    const getRoleName = (roleId) => {
+        const role = roles.find(role => role._id === roleId);
+        return role ? role.name : 'Desconocido';
+    };
+
+    const getColor = (roleId) => {
+        const role = roles.find(role => role._id === roleId);
+        return role ? role.color : 'default';
+    };
+
     return (
-        <div className="usuarios-page">
+        <Layout>
             <Sidebar />
-            <div className="usuarios-content">
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                <Divider />
                 <h1>Usuarios</h1>
                 <div className='usuarios-container'>
                     <Table
@@ -129,8 +140,8 @@ const Usuarios = () => {
                         <BotonesCrudUsuario selectedUserId={selectedUserId} />
                     )}
                 </div>
-            </div>
-        </div>
+            </Content>
+        </Layout>
     );
 };
 
